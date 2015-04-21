1e8e05954aeb5efa51ece6280fee72370de69637
matthew_kercheval <mkercheval@gmail.com>
2015-04-21 18:21:47 -0500

    starting document db work

---
+++ v1/Mantle/Mantle.Document/Interfaces/IDocumentClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Mantle.Document.Interfaces
+{
+    public interface IDocumentDatabaseClient
+    {
+        void DeleteDatabase(string databaseName);
+        bool DatabaseExists(string entityId, string partitionId);
+    }
+}
---
+++ v1/Mantle/Mantle.Document/Interfaces/IDocumentDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Mantle.Document.Interfaces
+{
+    public interface IDocumentDatabase
+    {
+       string Id { get; set; }
+       DateTime Timestamp { get;}
+    }
+}
